a7853c8e2032c3bb2dee911433e1efcca483fa0e
DarrelRajendraK <144996959+DarrelRajendraK@users.noreply.github.com>
2024-02-01 16:41:14 +0700

    Add files via upload

---
+++ remedialPBO/remedialpbo/src/main/java/com/laundrykenangan/interfaceJDBC.java
@@ -0,0 +1,12 @@
+package com.remedialpbo;
+
+public interface interfaceJDBC {
+    public void masukandatapembelian();
+
+    public void updatedatapembelian();
+
+    public void hapusdatapembelian();
+
+    public void tampilkanMenu();
+
+}
